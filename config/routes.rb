54d7b66abffd358a5e1ba368e44aa8272f245e64
saori.matsuura <sao.matsuura@gmail.com>
2019-06-23 16:01:01 +0000

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
-  resources :tasks
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  root to: 'toppages#index'
+  # resources :tasks
+  resources :tasks, only: [:show, :new, :create, :destroy, :update, :edit]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
