b0f2c4f986d284e88b7e952e428e902a4f80b5fa
saori.matsuura <sao.matsuura@gmail.com>
2019-06-25 07:59:01 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,12 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in
+  before_action :require_user_logged_in, only: [:destroy, :edit, :update, :show, :create, :new]
   before_action :correct_user, only: [:destroy, :edit, :update, :show]
 
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
 
   def show
